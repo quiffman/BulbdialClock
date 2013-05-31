c73baa2c2052f0ea8b76e915bd8ba4eef74f9449
William Phelps <89y8vvunae@snkmail.com>
2013-05-30 17:55:56 -0700

    use precomputed FadeConv table - smaller size

--- BulbdialClock.ino
+++ BulbdialClock.ino
@@ -103,6 +103,17 @@ Todo:
 
 #define tempfade 63
 #define fadeGamma 1.3
+#define fadeMax 63.0
+
+// logarithmic conversion table for LED fading - gamma 1.3, max 63 - 30 May 2013 W B Phelps
+byte FadeConv[] = {0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6,
+ 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14,
+ 15, 15, 15, 15, 16, 16, 16, 17, 17, 17, 18, 18, 18, 19, 19, 19, 19, 20, 20, 20, 21, 21, 21, 22, 22, 22, 23, 
+ 23, 23, 24, 24, 24, 25, 25, 25, 26, 26, 26, 27, 27, 27, 28, 28, 28, 29, 29, 29, 30, 30, 30, 31, 31, 31, 
+ 32, 32, 32, 33, 33, 33, 34, 34, 35, 35, 35, 36, 36, 36, 37, 37, 37, 38, 38, 39, 39, 39, 40, 40, 40, 
+ 41, 41, 41, 42, 42, 43, 43, 43, 44, 44, 44, 45, 45, 46, 46, 46, 47, 47, 48, 48, 48, 49, 49, 49, 
+ 50, 50, 51, 51, 51, 52, 52, 53, 53, 53, 54, 54, 55, 55, 55, 56, 56, 57, 57, 57, 58, 58, 59, 59, 59, 
+ 60, 60, 61, 61, 61, 62, 62, 63, 63};
 
 void TakeHigh(byte LEDline)
 {
@@ -218,7 +229,7 @@ boolean getPCtime() {
   // if time sync available from serial port, update time and return true
   while(Serial.available() >=  TIME_MSG_LEN ){  // time message consists of a header and ten ascii digits
     if( Serial.read() == TIME_HEADER ) {
-      atime_t pctime = 0;
+      time_t pctime = 0;
       for(int i=0; i < TIME_MSG_LEN -1; i++){
         char c= Serial.read();
         if( c >= '0' && c <= '9'){
@@ -327,8 +338,6 @@ byte h0, h1, h2, h3, h4, h5;
 byte l0, l1, l2, l3, l4, l5;
 byte d0, d1, d2, d3, d4, d5;
 byte HrFade1, HrFade2, MinFade1, MinFade2, SecFade1, SecFade2;
-
-byte FadeConv[200];
 
 void ApplyDefaults (void) {
   /*
@@ -810,7 +819,7 @@ void setup()  // run once, when the sketch starts
 
   // Compute logarithmic brightness values for fade just once - doing it on the fly causes flicker
   for(int i=0; i <= 200; i++) {
-    FadeConv[i] = pow(i/200.0,fadeGamma)*64.0;
+    FadeConv[i] = pow(i/200.0,fadeGamma)*fadeMax;
   }
 
 }  // End Setup
